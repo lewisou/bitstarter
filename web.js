818e016eaadc49696035158cd98e2b6129ec28cb
Lewis Z <dev@lewisou.com>
2013-07-07 12:34:45 +0000

    index.html

--- web.js
+++ web.js
@@ -2,8 +2,19 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var read_file = function (file_name) {
+    var fs = require ('fs');
+    var rs = '';
+    fs.readFileSync ('/home/dev/startupeng/bitstarter/' + file_name, function (err, data) {
+	if (err) throw err;
+	rs = data;
+    });
+    return ts.toString ('utf8');
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+    var html = read_file ('index.html');
+    response.send(html);
 });
 
 var port = process.env.PORT || 5000;
